1cff057954991557a9cce7aed3598658bdcb16dc
matheus dornelles <mdmaia91@gmail.com>
2023-09-30 01:01:21 -0300

    fix:
    - Removendo query não utilizada.

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,7 +1,2 @@
-CREATE TABLE Pessoa (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(255),
-    data_nascimento DATE
-);
 INSERT INTO Pessoa (nome, data_nascimento) VALUES ('Matheus', '1991-02-01');
 INSERT INTO Endereco (logradouro, cidade, pessoa_id) VALUES ('Rua Exemplo 123', 'Cidade Exemplo', @pessoaId);
